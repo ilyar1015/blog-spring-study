bd4e4e378a86fec655a9325705ab38c6a4dcb445
ilyar <ilyar1015@hotmail.com>
2020-03-09 14:10:38 +0800

    Initial commit

---
+++ src/main/java/com/my/blogspringstudy/BlogSpringStudyApplication.java
@@ -0,0 +1,13 @@
+package com.my.blogspringstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSpringStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogSpringStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/my/blogspringstudy/BlogSpringStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.blogspringstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSpringStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
