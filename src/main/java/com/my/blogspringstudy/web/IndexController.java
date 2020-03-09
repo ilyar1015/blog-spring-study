4037c2295e86d63b2d579dd98c4b8cc09c8b7180
ilyar <ilyar1015@hotmail.com>
2020-03-09 19:46:30 +0800

    thymeleaf

--- src/main/java/com/my/blogspringstudy/web/IndexController.java
+++ src/main/java/com/my/blogspringstudy/web/IndexController.java
@@ -4,6 +4,7 @@ package com.my.blogspringstudy.web;
 import com.my.blogspringstudy.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
@@ -11,10 +12,16 @@ public class IndexController {
     @GetMapping("/")
     public String index(){
 //        int i=9/0;
-        String blog = null;
-        if(blog == null){
-            throw new NotFoundException("博客不存在");
-        }
+//        String blog = null;
+//        if(blog == null){
+//            throw new NotFoundException("博客不存在");
+//        }
+        System.out.println("------------index----------------");
         return "index";
+    }
+
+    @GetMapping("/blog")
+    public String blog(){
+        return "blog";
     }
 }
