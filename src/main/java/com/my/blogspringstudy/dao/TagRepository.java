de379eba1cf9803f16505a690b1ad6133cc44477
ilyar <ilyar1015@hotmail.com>
2020-03-17 18:04:28 +0800

    add blog

--- src/main/java/com/my/blogspringstudy/dao/TagRepository.java
+++ src/main/java/com/my/blogspringstudy/dao/TagRepository.java
@@ -3,6 +3,8 @@ package com.my.blogspringstudy.dao;
 import com.my.blogspringstudy.po.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TagRepository extends JpaRepository<Tag, Long> {
     Tag findByName(String name);
 }
